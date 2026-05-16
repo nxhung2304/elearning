012e2f0365c15a514677257cf10f2425e68cfc8e
Hung <mongtamquoc2015@gmail.com>
2026-05-16 19:01:23 +0700

    refactor code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,17 @@ module ApplicationHelper
   def breadcrumbs
     @breadcrumbs || []
   end
+
+  def status_badge_color(status)
+    case status
+    when "active"
+      "bg-green-100 text-green-800"
+    when "inactive"
+      "bg-gray-100 text-gray-800"
+    when "deleted"
+      "bg-red-100 text-red-800"
+    else
+      "bg-gray-100 text-gray-800"
+    end
+  end
 end
